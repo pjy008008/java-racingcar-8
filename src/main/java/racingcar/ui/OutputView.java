ce73a56302c9c65e5a30977a163370aa8e1f83e8
Junyou Park <pjy008008@gmail.com>
2025-10-24 22:17:14 +0900

    refactor(OutputView): 하드코딩 문자열 상수화

--- src/main/java/racingcar/ui/OutputView.java
+++ src/main/java/racingcar/ui/OutputView.java
@@ -6,24 +6,29 @@ import racingcar.domain.Cars;
 import java.util.stream.Collectors;
 
 public class OutputView {
+    private static final String DELIMITER = ",";
+    private static final String WINNER_MESSAGE = "최종 우승자 : ";
+    private static final String START_MESSAGE = "\n실행 결과";
+    private static final String NAME_POSITION_SEPARATOR = " : ";
+    private static final String POSITION_MARK = "-";
+
     public void printWinners(Cars winners) {
         String result = winners.getCarList()
                 .stream()
                 .map(Car::getName)
-                .collect(Collectors.joining(","));
-        System.out.println("최종 우승자 : " + result);
+                .collect(Collectors.joining(DELIMITER));
+        System.out.println(WINNER_MESSAGE + result);
     }
 
     public void printStartMessage() {
-        System.out.println();
-        System.out.println("실행 결과");
+        System.out.println(START_MESSAGE);
     }
 
     public void printCarsAndPositions(Cars cars) {
         for (Car car : cars.getCarList()) {
-            System.out.print(car.getName()+" : ");
+            System.out.print(car.getName() + NAME_POSITION_SEPARATOR);
             int position = car.getPosition();
-            System.out.println("-".repeat(position));
+            System.out.println(POSITION_MARK.repeat(position));
         }
         System.out.println();
     }

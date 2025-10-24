67ac64d05fceaba3b29596045b82ddd5e8cb1770
Junyou Park <pjy008008@gmail.com>
2025-10-24 17:38:41 +0900

    feat(InputValidator): 자동차 이름 중복 검증 기능 구현

--- src/main/java/racingcar/validation/InputValidator.java
+++ src/main/java/racingcar/validation/InputValidator.java
@@ -1,17 +1,21 @@
 package racingcar.validation;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class InputValidator {
     private static final int MAX_NAME_LENGTH = 5;
     private static final String ERROR_CAR_NAME_EMPTY = "자동차 이름이 빈칸입니다.";
     private static final String ERROR_CAR_NAME_LENGTH_EXCEEDED = "자동차 이름은 " + MAX_NAME_LENGTH + "자를 초과할 수 없습니다.";
+    private static final String ERROR_CAR_NAME_DUPLICATE = "자동차 이름은 중복될 수 없습니다.";
     private static final String ERROR_ATTEMPT_NOT_A_NUMBER = "시도 횟수는 숫자여야 합니다.";
     private static final String ERROR_ATTEMPT_NOT_POSITIVE = "시도 횟수는 양수여야 합니다.";
 
     public void validateCarName(List<String> carNames) {
         validateCarNameLength(carNames);
         validateCarNameIsEmpty(carNames);
+        validateCarNameDuplicate(carNames);
     }
 
     private void validateCarNameIsEmpty(List<String> carNames) {
@@ -25,6 +29,15 @@ public class InputValidator {
         for (String carName : carNames) {
             if (carName.length() > MAX_NAME_LENGTH) {
                 throw new IllegalArgumentException(ERROR_CAR_NAME_LENGTH_EXCEEDED);
+            }
+        }
+    }
+
+    private void validateCarNameDuplicate(List<String> carNames) {
+        Set<String> uniqueNames = new HashSet<>();
+        for (String carName : carNames) {
+            if (!uniqueNames.add(carName)) {
+                throw new IllegalArgumentException(ERROR_CAR_NAME_DUPLICATE);
             }
         }
     }

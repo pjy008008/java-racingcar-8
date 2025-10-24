2c3df2ee584a93e3d540a25606779015288e3629
Junyou Park <pjy008008@gmail.com>
2025-10-24 16:36:30 +0900

    feat(OutputView): 경주 결과 및 현황을 출력하는 뷰 클래스 구현
    - 최종 우승자를 출력하는 printWinners 함수 구현
    - 각 자동차의 이름과 이동 거리를 출력하는 printCarsAndPositions 함수 구현

---
+++ src/main/java/racingcar/ui/OutputView.java
@@ -0,0 +1,29 @@
+package racingcar.ui;
+
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+
+import java.util.stream.Collectors;
+
+public class OutputView {
+    public void printWinners(Cars winners) {
+        String result = winners.getCarList()
+                .stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(","));
+        System.out.println("최종 우승자 : " + result);
+    }
+
+    public void printStartMessage() {
+        System.out.println("실행 결과");
+    }
+
+    public void printCarsAndPositions(Cars cars) {
+        for (Car car : cars.getCarList()) {
+            System.out.print(car.getName()+" : ");
+            int position = car.getPosition();
+            System.out.println("-".repeat(position));
+        }
+        System.out.println();
+    }
+}
